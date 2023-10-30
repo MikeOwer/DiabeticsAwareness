484b043998272cbdef8aa084211a4f3b2c382a46
Miguel <yugimio@hotmail.com>
2023-10-30 10:27:07 -0600

    Añadí el color principal y el de fondo

--- diabetic_app/lib/main.dart
+++ diabetic_app/lib/main.dart
@@ -2,8 +2,9 @@ import 'package:diabetic_app/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'my_classes/notifications.dart';
+import 'package:diabetic_app/ProyectColors.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await Notifications().initNotifications();
@@ -13,18 +14,17 @@ Future<void> main() async{
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        primarySwatch: new ProyectColors()
+            .primaryMaterialColor, //Se utiliza el color creado para la aplicación
+        scaffoldBackgroundColor: new ProyectColors().backgroundColor,
       ),
       home: const WidgetTree(),
     );
   }
 }
-
-
