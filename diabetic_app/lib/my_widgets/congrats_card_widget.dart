7ea96ce679f5ef5ef4ed378fafcb436415ae3a2d
Miguel <yugimio@hotmail.com>
2023-12-04 11:28:51 -0600

    Se cambiaron de momento para observación, pero ya no son útiles para la app

--- diabetic_app/lib/my_widgets/congrats_card_widget.dart
+++ diabetic_app/lib/my_widgets/congrats_card_widget.dart
@@ -6,8 +6,7 @@ import 'package:share_plus/share_plus.dart';
 
 import '../my_classes/auth.dart';
 
-class CongratsCardWidget extends StatelessWidget{
-
+class CongratsCardWidget extends StatelessWidget {
   final User? user = Auth().currentUser;
   QuizController quizController = QuizController.getInstance();
   int level = 0;
@@ -15,42 +14,33 @@ class CongratsCardWidget extends StatelessWidget{
   CongratsCardWidget({required this.level});
 
   Future shareWithFacebook(int level) async {
-    final text = '¡He completado con éxito el nivel $level del Quiz de DiabeticAwareness!.';
+    final text =
+        '¡He completado con éxito el nivel $level del Quiz de DiabeticAwareness!.';
     await Share.share(text);
     updateProgress();
   }
 
   void updateProgress() {
-    if((quizController.quizProgress.getMaxLevel() == quizController.quizProgress.getHealthyLevels()
-    && quizController.quizProgress.getMaxLevel() > this.level)
-    || quizController.quizProgress.getMaxLevel() == 0){
-      quizController.quizProgress.increaseMaxLevel();
-      quizController.quizProgress.increaseHealthyLevels();
-    } else if (quizController.quizProgress.getMaxLevel() > quizController.quizProgress.getHealthyLevels()){
-      quizController.quizProgress.increaseHealthyLevels();
-    }
+    quizController.quizProgress.increaseMaxLevel();
     quizController.updateProgressJSONFile();
     quizController.resetQuiz();
   }
-  void goBackToQuizMenu(BuildContext context){
+
+  void goBackToQuizMenu(BuildContext context) {
     updateProgress();
     Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => QuizLobbyPage())
-    );
+        context, MaterialPageRoute(builder: (context) => QuizLobbyPage()));
   }
 
   Widget _facebookButton() {
     return ElevatedButton(
       onPressed: () => shareWithFacebook(this.level),
-      child: Text(
-          'Compartir mi logro'
-      ),
+      child: Text('Compartir mi logro'),
     );
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Center(
       child: SizedBox(
         height: MediaQuery.of(context).size.height * 0.35,
@@ -59,12 +49,15 @@ class CongratsCardWidget extends StatelessWidget{
             padding: EdgeInsets.all(16),
             child: Column(
               children: [
-                Text("¡Felicidades!\n Completaste exitosamente el nivel.",
+                Text(
+                  "¡Felicidades!\n Completaste exitosamente el nivel.",
                   style: TextStyle(fontSize: 24),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 40,),
-                user != null? _facebookButton() : SizedBox(),
+                SizedBox(
+                  height: 40,
+                ),
+                user != null ? _facebookButton() : SizedBox(),
                 GestureDetector(
                   onTap: () => goBackToQuizMenu(context),
                   child: Text("Regresar al menú"),
---
+++ diabetic_app/lib/my_widgets/small_widgets/question_buttons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuestionButton extends StatelessWidget {
+  const QuestionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
